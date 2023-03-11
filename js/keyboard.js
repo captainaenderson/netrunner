f7e0941d7d8f248add6b0088942c5c0eab48fe0a
Ann-Kathrin Wohlrab <ann-kathrinwohlrab@imacvonnkathrin.speedport.ip>
2023-03-11 09:30:54 +0100

    solve routing issues

---
+++ js/keyboard.js
@@ -0,0 +1,11 @@
+class Keys {
+    constructor(x, y, ctx){
+        this.height = 50;
+        this.width = 50;
+        this.x = x;
+        this.y = y;
+        this.ctx = ctx;
+    }
+}
+
+export default Keys;
---
+++ js/main.js
@@ -0,0 +1,9 @@
+import Keys from "./keyboard.js";
+
+
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+console.log('hello')
+
